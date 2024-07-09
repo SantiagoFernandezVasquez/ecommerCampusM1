a32680cdb6d97dc3b096363e46800597879afc34
SantiagoFernandezVasquez <160640951+SantiagoFernandezVasquez@users.noreply.github.com>
2024-07-09 06:48:39 -0500

    chore: 🚑 Cambio de los headers de la Api
    Actualicé el header de la Api para que muestre la informacion en mi pagina web

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'f93985fc69msh7f82878d76739e6p1e1a32jsn424f7363a09c',
+    'x-rapidapi-key': 'd9c74741b2msh7c9852eacde840ep1e1fbejsn54116a831153',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
  }
